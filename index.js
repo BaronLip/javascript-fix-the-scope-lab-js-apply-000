0049d9a22313fea67a64bbbd0f457027f363bef4
baronlip <baronlip@gmail.com>
2019-04-29 02:13:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,19 +4,29 @@ function myAnimal() {
   return animal
 }
 
+// How can we make sure that this function
+// and the above function both pass?
+// P.S.: You can't just hard-code 'cat' below
+
 function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
+  var animal = "cat"
   return animal
 }
 
-function add2(n) {
-  return n + two
 
-  // Feel free to move things around!
+
+
+// Feel free to move things around!
+
+n = Math.floor(Math.random() * 1000)
+
+function add2(n) {
   const two = 2
+  return n + two
 }
+
+
+
 
 var funkyFunction = function() {
   return function() {
@@ -26,4 +36,32 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+
+
+function funkyFunction() {
+  var funky = "FUNKY!"
+  return function theFunk() {
+    return funky;
+  }
+}
+
+var funky = theFunk()
+
+
+
+// var theFunk = funkyFunction();
+// theFunk()
+  
+  
+// describe('funkyFunction()', function() {
+//     it('returns a function', function() {
+//       expect(typeof window.funkyFunction()).toEqual('function')
+//     })
+//   })
+
+//   describe('theFunk', function() {
+//     it('is "FUNKY!"', function() {
+//       expect(window.theFunk).toEqual('FUNKY!')
+//     })
+//   })
+
